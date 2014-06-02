31eda820e30a0b8fd9d21fb61153a565071689be
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-06-02 20:58:25 +0200

    change parent detection

--- browscap.js
+++ browscap.js
@@ -5,6 +5,8 @@ exports.setJson = function (filename) {
 };
 
 exports.getBrowser = function (userAgent) {
+  var browsers, patterns, re, key, found, matches, browsersindex;
+
   patterns = require(jsonfile).patterns;
   browsers = require(jsonfile).browsers;
 
@@ -14,15 +16,15 @@ exports.getBrowser = function (userAgent) {
     re = new RegExp(pattern, 'i');
 
     if (re.test(userAgent)) {
-      key     = patterns[pattern];
-      found   = false;
+      key = patterns[pattern];
+      found = false;
       matches = userAgent.match(re);
 
       if (matches.length === 1) {
         browsersindex = key;
         found = true;
       } else {
-        matchString = '@' + matches.join('|');
+        var matchString = '@' + matches.join('|');
 
         if (key[matchString]) {
           browsersindex = key[matchString];
@@ -31,15 +33,15 @@ exports.getBrowser = function (userAgent) {
       }
 
       if (found && browsers[browsersindex]) {
-        browser = [userAgent, pattern.toLowerCase().trim()];
-        browserData = JSON.parse(browsers[browsersindex]);
+        var browser = [userAgent, pattern.toLowerCase().trim()];
+        var browserData = JSON.parse(browsers[browsersindex]);
 
         for (var property in browserData) {
           browser[property] = browserData[property];
         }
 
-        while (browserData['3']) {
-          browserData = JSON.parse(browsers[browserData[3]]);
+        while (browserData['Parent']) {
+          browserData = JSON.parse(browsers[browserData['Parent']]);
 
           for (var property in browserData) {
             if (!browser[property]) {
@@ -48,20 +50,12 @@ exports.getBrowser = function (userAgent) {
           }
         }
 
-        if (browser['3']) {
-          userAgents = require(jsonfile).userAgents;
-          browser['3'] = userAgents[browser['3']];
+        if (browser['Parent']) {
+          var userAgents = require(jsonfile).userAgents;
+          browser['Parent'] = userAgents[browser['Parent']];
         }
 
-        ua = {};
-        properties = require(jsonfile).properties;
-
-        for (var propertyNumber in browser) {
-          propertyName = properties[propertyNumber];
-          ua[propertyName] = browser[propertyNumber];
-        }
-
-        return ua;
+        return browser;
       }
     }
   }
