0a2c10197a5f6d87153f17f959068690c6ce6c23
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-01-25 13:44:23 +0100

    add new test

---
+++ test/issue-544.js
@@ -0,0 +1,38 @@
+var assert = require('assert'),
+  browscap = require('../browscap.js'),
+  browser;
+
+suite('checking for issue 544', function () {
+  test('Test', function () {
+    browser = browscap.getBrowser("Mozilla/5.0 (Linux;U;Android 4.4.2;zh-cn;Lenovo A3300-HV Build/KOT49H) AppleWebkit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/537.306;");
+
+    assert.strictEqual(browser['Browser'], 'Android WebView');
+    assert.strictEqual(browser['Browser_Type'], 'Browser');
+    assert.strictEqual(browser['Browser_Bits'], '32');
+    assert.strictEqual(browser['Browser_Maker'], 'Google Inc');
+    assert.strictEqual(browser['Version'], '4.0');
+    assert.strictEqual(browser['MajorVer'], '4');
+    assert.strictEqual(browser['MinorVer'], '0');
+    assert.strictEqual(browser['Platform'], 'Android');
+    assert.strictEqual(browser['Platform_Version'], '4.4');
+    assert.strictEqual(browser['Platform_Bits'], '32');
+    assert.strictEqual(browser['Platform_Maker'], 'Google Inc');
+    assert.strictEqual(browser['isMobileDevice'], true);
+    assert.strictEqual(browser['isTablet'], false);
+    assert.strictEqual(browser['Crawler'], false);
+    assert.strictEqual(browser['JavaScript'], true);
+    assert.strictEqual(browser['Cookies'], true);
+    assert.strictEqual(browser['Frames'], true);
+    assert.strictEqual(browser['IFrames'], true);
+    assert.strictEqual(browser['Tables'], true);
+    assert.strictEqual(browser['Device_Name'], 'TAB A7-30');
+    assert.strictEqual(browser['Device_Maker'], 'Lenovo');
+    assert.strictEqual(browser['Device_Pointing_Method'], 'touchscreen');
+    assert.strictEqual(browser['Device_Code_Name'], 'A3300-HV');
+    assert.strictEqual(browser['Device_Brand_Name'], 'Lenovo');
+    assert.strictEqual(browser['RenderingEngine_Name'], 'Blink');
+    assert.strictEqual(browser['RenderingEngine_Version'], 'unknown');
+    assert.strictEqual(browser['RenderingEngine_Maker'], 'Google Inc');
+  });
+});
+
