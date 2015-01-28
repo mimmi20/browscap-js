80f3dc47d1caa1cfaa600ed3c0c393df6a5ea48f
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-01-28 22:54:56 +0100

    update browscap.js to use actual browscap.json

--- browscap.js
+++ browscap.js
@@ -5,10 +5,9 @@ exports.setJson = function (filename) {
 };
 
 exports.getBrowser = function (userAgent) {
-  var browsers, patterns, re, found, matches, browsersindex;
+  var patterns, re;
 
-  patterns = require(jsonfile).patterns;
-  browsers = require(jsonfile).browsers;
+  patterns = require(jsonfile);
 
   // Test user agent against each browser regex
   for (var pattern in patterns) {
@@ -16,66 +15,50 @@ exports.getBrowser = function (userAgent) {
       continue;
     }
 
-    re = new RegExp(pattern.replace(/@/g, ''), 'i');
+    if ('GJK_Browscap_Version' === pattern || 'comments' === pattern) {
+      continue;
+    }
+
+    re = new RegExp(pattern.replace(/@/g, '').replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"), 'i');
 
     if (re.test(userAgent)) {
-      patternData = patterns[pattern];
-      found = false;
-      matches = userAgent.match(re);
-      matches.shift();
+      var browser = {
+        browser_name: userAgent,
+        browser_name_regex: pattern.toLowerCase().trim()
+      };
+      var browserData = JSON.parse(patterns[pattern]);
 
-      if (matches.length === 1) {
-        browsersindex = patternData;
-        found = true;
-      } else {
-        var matchString = '@' + matches.join('|');
-
-        if (patternData[matchString]) {
-          browsersindex = patternData[matchString];
-          found = true;
+      for (var property in browserData) {
+        if (!browserData.hasOwnProperty(property)) {
+          continue;
         }
+
+        browser[property] = browserData[property];
       }
 
-      if (found && browsers[browsersindex]) {
-        var browser = {
-          browser_name: userAgent,
-          browser_name_regex: pattern.toLowerCase().trim()
-        };
-        var browserData = JSON.parse(browsers[browsersindex]);
+      var browserParentData = browserData;
 
-        for (var property in browserData) {
-          if (!browserData.hasOwnProperty(property)) {
+      while (browserParentData['Parent']) {
+        browserParentData = JSON.parse(patterns[browserParentData['Parent']]);
+
+        for (var propertyParent in browserParentData) {
+          if (!browserParentData.hasOwnProperty(propertyParent)) {
             continue;
           }
 
-          browser[property] = browserData[property];
-        }
-
-        var browserParentData = browserData;
-
-        while (browserParentData['Parent']) {
-          browserParentData = JSON.parse(browsers[browserParentData['Parent']]);
-
-          for (var propertyParent in browserParentData) {
-            if (!browserParentData.hasOwnProperty(propertyParent)) {
-              continue;
-            }
-
-            if (browser.hasOwnProperty(propertyParent)) {
-              continue;
-            }
-
-            browser[propertyParent] = browserParentData[propertyParent];
+          if (browser.hasOwnProperty(propertyParent)) {
+            continue;
           }
-        }
 
-        if (browser['Parent']) {
-          var userAgents = require(jsonfile).userAgents;
-          browser['Parent'] = userAgents[browser['Parent']];
+          browser[propertyParent] = browserParentData[propertyParent];
         }
-
-        return browser;
       }
+
+      if (browser['Parent']) {
+        browser['Parent'] = patterns[browser['Parent']];
+      }
+
+      return browser;
     }
   }
 
