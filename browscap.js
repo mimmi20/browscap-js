02233b4d8322053b15df5fc18c75983f5b48d6a4
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-02-12 23:06:46 +0100

    Revert "update browscap.js to use actual browscap.json"
    This reverts commit 80f3dc47d1caa1cfaa600ed3c0c393df6a5ea48f.

--- browscap.js
+++ browscap.js
@@ -5,9 +5,10 @@ exports.setJson = function (filename) {
 };
 
 exports.getBrowser = function (userAgent) {
-  var patterns, re;
+  var browsers, patterns, re, found, matches, browsersindex;
 
-  patterns = require(jsonfile);
+  patterns = require(jsonfile).patterns;
+  browsers = require(jsonfile).browsers;
 
   // Test user agent against each browser regex
   for (var pattern in patterns) {
@@ -15,50 +16,66 @@ exports.getBrowser = function (userAgent) {
       continue;
     }
 
-    if ('GJK_Browscap_Version' === pattern || 'comments' === pattern) {
-      continue;
-    }
-
-    re = new RegExp(pattern.replace(/@/g, '').replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"), 'i');
+    re = new RegExp(pattern.replace(/@/g, ''), 'i');
 
     if (re.test(userAgent)) {
-      var browser = {
-        browser_name: userAgent,
-        browser_name_regex: pattern.toLowerCase().trim()
-      };
-      var browserData = JSON.parse(patterns[pattern]);
+      patternData = patterns[pattern];
+      found = false;
+      matches = userAgent.match(re);
+      matches.shift();
 
-      for (var property in browserData) {
-        if (!browserData.hasOwnProperty(property)) {
-          continue;
-        }
+      if (matches.length === 1) {
+        browsersindex = patternData;
+        found = true;
+      } else {
+        var matchString = '@' + matches.join('|');
 
-        browser[property] = browserData[property];
-      }
-
-      var browserParentData = browserData;
-
-      while (browserParentData['Parent']) {
-        browserParentData = JSON.parse(patterns[browserParentData['Parent']]);
-
-        for (var propertyParent in browserParentData) {
-          if (!browserParentData.hasOwnProperty(propertyParent)) {
-            continue;
-          }
-
-          if (browser.hasOwnProperty(propertyParent)) {
-            continue;
-          }
-
-          browser[propertyParent] = browserParentData[propertyParent];
+        if (patternData[matchString]) {
+          browsersindex = patternData[matchString];
+          found = true;
         }
       }
 
-      if (browser['Parent']) {
-        browser['Parent'] = patterns[browser['Parent']];
-      }
+      if (found && browsers[browsersindex]) {
+        var browser = {
+          browser_name: userAgent,
+          browser_name_regex: pattern.toLowerCase().trim()
+        };
+        var browserData = JSON.parse(browsers[browsersindex]);
 
-      return browser;
+        for (var property in browserData) {
+          if (!browserData.hasOwnProperty(property)) {
+            continue;
+          }
+
+          browser[property] = browserData[property];
+        }
+
+        var browserParentData = browserData;
+
+        while (browserParentData['Parent']) {
+          browserParentData = JSON.parse(browsers[browserParentData['Parent']]);
+
+          for (var propertyParent in browserParentData) {
+            if (!browserParentData.hasOwnProperty(propertyParent)) {
+              continue;
+            }
+
+            if (browser.hasOwnProperty(propertyParent)) {
+              continue;
+            }
+
+            browser[propertyParent] = browserParentData[propertyParent];
+          }
+        }
+
+        if (browser['Parent']) {
+          var userAgents = require(jsonfile).userAgents;
+          browser['Parent'] = userAgents[browser['Parent']];
+        }
+
+        return browser;
+      }
     }
   }
 
